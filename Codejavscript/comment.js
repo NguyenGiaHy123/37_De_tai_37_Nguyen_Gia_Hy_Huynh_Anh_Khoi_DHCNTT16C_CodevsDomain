a0ca81f8abef12365f9f364d4581fddf1c3a1361
NguyenGiaHy123 <89922176+NguyenGiaHy123@users.noreply.github.com>
2022-04-27 00:05:43 +0700

    fafadfa

---
+++ Codejavscript/comment.js
@@ -0,0 +1,57 @@
+$(document).ready(()=>{
+   var coment_user=[];
+   $('.send_comnent').click(()=>{
+    var noidung_comment=$('.text_coment').val();
+    var  user_imgs="../../imgweb/user.png";
+    var namuser="Nguyễn Gia Hy"
+    var dates=new Date();
+    
+       var comment={
+           name:"Nguyễn Gia Hy",
+           user_img:user_imgs,
+           date:dates,
+           contentext:noidung_comment
+           
+       }
+       coment_user.push(comment);
+  
+       localStorage.setItem('dateuser_comnets',JSON.stringify(coment_user))
+     console.log(  JSON.parse(localStorage.getItem('dateuser_comnets')))
+       var html=` <div class=" contaner_commnet mt-5">
+       <div class="user_comment">
+           <img src=${user_imgs} alt="">
+           <div class="coment_right">
+               <h6>${namuser}</h6>
+               <p>${dates}</p>
+           </div>
+          
+       </div>
+       <p class="noidung_comment">${noidung_comment}</p>
+   </div>`
+    $('.body_coment').append(html);
+   })
+
+   JSON.parse(localStorage.getItem('dateuser_comnets')).forEach(coments => {
+       
+    var html1=` <div class=" contaner_commnet mt-5">
+    <div class="user_comment">
+        <img src=${coments.user_img} alt="">
+        <div class="coment_right">
+            <h6>${coments.name}</h6>
+            <p>${coments.date}</p>
+        </div>
+       
+    </div>
+    <p class="noidung_comment">${coments.contentext}</p>
+</div>`
+
+        $('.body_coment').append(html1);
+   });
+
+
+  
+  
+    
+
+
+})
